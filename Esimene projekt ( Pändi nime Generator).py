709182cad5965c5bc46ba14318a6bee614c04f8f
RihoSepp <126762252+Stepsepp@users.noreply.github.com>
2023-12-01 02:06:59 +0200

    pändi generaatori nime
    Esimese leongu õpitul lõin Pöndi nime generaatori mis küsib inimeselt lemmik linna ja sõna.

---
+++ Esimene projekt ( Pändi nime Generator).py
@@ -0,0 +1,9 @@
+# Loome sissejuhatuse 
+print("Tere Tulemast Pändi nime loomisse")
+# Küsime inimeselt mis linn on tema lemmik linn
+Linn = input("Mis on su lemmik linn?\n")
+# Küsime inimese lemmik sõna 
+Sõna = input("Mis on su lemmik sõna?\n")
+# Liidame tulemused kokku Linn + Sõna
+print("Su pändi nimeks saab olema " + Linn + " " +  Sõna )
+# Lisasime ka \n selleks et kui me küsime linna või sõna kohta saab vastuse kirjutada uuele reale, mitte küsimusel järgi
