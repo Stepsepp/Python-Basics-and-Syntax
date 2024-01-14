610693c417d143dd5b6965767c969d06353e7460
RihoSepp <126762252+Stepsepp@users.noreply.github.com>
2024-01-14 19:29:40 +0200

    Day 16 Intermediate - Object Oriented Programming
    Intermediate - Object Oriented Programming ( OOP)

---
+++ main.py
@@ -0,0 +1,24 @@
+# Selleks et antud koodi käivitada eemalda trellid 3-11. ( Kui eemdalda trellid jälgi et kood oleks õiges reas)
+
+#from  turtle import Turtle, Screen
+#timmy = Turtle()
+#print(timmy)
+#timmy.shape("turtle")
+#timmy.color("Red")
+#timmy.forward(300)
+#my_screen = Screen()
+#print(my_screen.canvheight)
+#my_screen.exitonclick()
+
+
+
+# Selleks et antud koodi käivitada eemdalda # 18-23 ( Kui eemdalda trellid jälgi et kood oleks õiges reas)
+
+#from prettytable import PrettyTable
+#table = PrettyTable()
+#table.add_column("Eesti_linnad",["Tallinn", "Tartu", "Pärnu"])
+#table.add_column("Type", ["Pealinn", "Tudengite_Linn", "Suve_Pealinn"])
+#print(table.align)
+#print(table)
+
+
